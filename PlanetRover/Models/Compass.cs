7fe4be17cd0dce3a06a9d8e452f21d0aab024d8e
Rádler Ákos <akos.radler@gmail.com>
2018-09-16 19:06:40 +0100

    Adding CompassExtensions

--- PlanetRover/Models/Compass.cs
+++ PlanetRover/Models/Compass.cs
@@ -12,4 +12,55 @@ namespace PlanetRover.Models
         S = 2,
         W = 3
     }
+
+    public static class CompassExtensions
+    {
+        public static Compass Invert(this Compass compass)
+        {
+            switch (compass)
+            {
+                case Compass.N:
+                    return Compass.S;
+                case Compass.E:
+                    return Compass.W;
+                case Compass.S:
+                    return Compass.N;
+                case Compass.W:
+                    return Compass.E;
+            }
+            return compass;
+        }
+
+        public static Compass TurnRight(this Compass compass)
+        {
+            switch (compass)
+            {
+                case Compass.N:
+                    return Compass.E;
+                case Compass.E:
+                    return Compass.S;
+                case Compass.S:
+                    return Compass.W;
+                case Compass.W:
+                    return Compass.N;
+            }
+            return compass;
+        }
+
+        public static Compass TurnLeft(this Compass compass)
+        {
+            switch (compass)
+            {
+                case Compass.N:
+                    return Compass.W;
+                case Compass.E:
+                    return Compass.N;
+                case Compass.S:
+                    return Compass.E;
+                case Compass.W:
+                    return Compass.S;
+            }
+            return compass;
+        }
+    }
 }
