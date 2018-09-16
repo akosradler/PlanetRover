9303ed9d85ddfbfcc835665c13012c7ed14ebe07
Rádler Ákos <akos.radler@gmail.com>
2018-09-16 18:38:00 +0100

    Adding RoverController and tests

---
+++ PlanetRover.Tests/Controllers/RoverControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanetRover.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlanetRover.Tests.Controllers
+{
+    public class RoverControllerTests
+    {
+        [Fact]
+        public async Task Land_ReturnsOK_WhenPosted()
+        {
+            //Arrange
+            var roverController = new RoverController();
+
+            //Act
+            var response = await roverController.Land();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(response.Result);
+        }
+    }
+}
---
+++ PlanetRover/Controllers/RoverController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanetRover.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoverController : ControllerBase
+    {
+        [HttpPost]
+        [Route("land")]
+        public async Task<ActionResult<bool>> Land()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
