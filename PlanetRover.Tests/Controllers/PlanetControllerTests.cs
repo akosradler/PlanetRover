0e52de164ac5b96728f7f4fb71c2dd2fb7fdd6de
Rádler Ákos <akos.radler@gmail.com>
2018-09-16 18:07:47 +0100

    Adding PlanetController and Test

---
+++ PlanetRover.Tests/Controllers/PlanetControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanetRover.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlanetRover.Tests.Controllers
+{
+    public class PlanetControllerTests
+    {
+        [Fact]
+        public async Task Get_ReturnsOK_WhenRequested()
+        {
+            //Arrange
+            var planetController = new PlanetController();
+
+            //Act
+            var response = await planetController.Get();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(response);
+        }
+    }
+}
---
+++ PlanetRover/Controllers/PlanetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PlanetRover.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanetController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/")]
+        public async Task<ActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
