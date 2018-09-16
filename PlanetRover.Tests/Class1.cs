82b3b08a077e3fb49ddf98ac25b278170969161d
Rádler Ákos <akos.radler@gmail.com>
2018-09-16 18:01:16 +0100

    HomeController and logging

--- PlanetRover.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PlanetRover.Tests
-{
-    public class Class1
-    {
-    }
-}
---
+++ PlanetRover/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PlanetRover.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Index()
+        {
+            return await Task.FromResult("Your API is running");
+        }
+    }
+}
